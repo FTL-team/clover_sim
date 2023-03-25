766b1e4531164e412f31d3ca8307d3d42e6fa07a
max <mrom06@ya.ru>
2023-03-25 15:11:02 +0300

    Add copy code button

--- src/web/src/TaskDescription.tsx
+++ src/web/src/TaskDescription.tsx
@@ -20,14 +20,54 @@ hljs.registerLanguage('c', c)
 hljs.registerLanguage('xml', xml)
 hljs.registerLanguage('yaml', yaml)
 
+function attachCopyButton(el: HTMLElement) {
+  let button = document.createElement('button')
+  button.innerText = 'Copied!'
+  button.className = styles['copy-button']
+  el.appendChild(button)
+  button.dataset['copied'] = 'false'
+
+  // Add a custom proprety to the code block so that the copy button can reference and match its background-color value.
+  el.style.setProperty(
+    '--hljs-theme-background',
+    window.getComputedStyle(el).backgroundColor
+  )
+
+  let curClearInterval = -1
+
+  button.onclick = function () {
+    if (!navigator.clipboard) return
+    button.innerText = ''
+    navigator.clipboard.writeText(el.innerText).then(() => {
+      button.dataset['copied'] = 'true'
+      button.innerText = 'Copied!'
+
+      clearInterval(curClearInterval)
+      curClearInterval = setTimeout(
+        () => (button.dataset['copied'] = 'false'),
+        1000
+      )
+    })
+  }
+}
+
+function attachCopyButtonToAll() {
+  let els = document.querySelectorAll<HTMLElement>('.hljs')
+  for (let el of els) {
+    if (el.querySelector('.' + styles['copy-button'])) continue
+    attachCopyButton(el)
+  }
+}
+
 const fetchReadme = () => fetchTaskFile()
 
 marked.setOptions({
   highlight: function (code, lang) {
     const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+    console.log(hljs.highlight(code, { language }).value)
     return hljs.highlight(code, { language }).value
   },
-  langPrefix: 'hljs language-',
+  langPrefix: styles['codeblock'] + ' hljs language-',
 })
 
 const Markdown: Component<{ children: string }> = (p) => {
@@ -41,6 +81,7 @@ const Markdown: Component<{ children: string }> = (p) => {
       })
       let safe = DOMPurify.sanitize(parsed)
       ref().innerHTML = safe
+      attachCopyButtonToAll()
     }
   })
 
